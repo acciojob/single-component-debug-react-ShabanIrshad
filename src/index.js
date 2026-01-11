9e4630c53b8ab32d68bf7b36219f0bc4cca06fc9
Shaban Irshad <shabanirshad408@gmail.com>
2026-01-11 13:35:19 +0530

    Assignment Completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
 
+import App from './components/App';
+import ReactDOM from 'react-dom/client';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
 
